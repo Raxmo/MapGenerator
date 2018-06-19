7aea7180484bcb55816c67ce0d91e6376fc0015b
Raxmo <maxsroe@gmail.com>
2018-06-19 05:26:06 -0600

    [UPDATE - BROKEN] 0000 - [COMPLETED] - optimized voronoi generation drastically
    [TODO]
    - create better biome colors
    - refactor code to put poisson oand voronoi into a function
    - create biome bias to create regions

--- Map Generator/Program.cs
+++ Map Generator/Program.cs
@@ -16,82 +16,6 @@ namespace MapGenerator
 	{
 		public static Random rng = new Random();
 
-		unsafe public static void Seed(int px, int py)
-		{
-			using (var seed = new Bitmap(256, 256, PixelFormat.Format24bppRgb))
-			{
-				// lock the array for direct access
-				var bitmapData = seed.LockBits(new Rectangle(0, 0, seed.Width, seed.Height), ImageLockMode.ReadWrite, PixelFormat.Format32bppPArgb);
-				// get the pointer
-				var scan0Ptr = (int*)bitmapData.Scan0;
-				// get the stride
-				var stride = bitmapData.Stride / 4;
-
-				for (var x = 0; x < seed.Width; x++)
-					for (var y = 0; y < seed.Height; y++)
-					{
-						var val = rng.Next(0, 255);
-						*(scan0Ptr + x + y * stride) = Color.FromArgb(255, val, val, val).ToArgb();
-					}
-
-				seed.UnlockBits(bitmapData);
-
-				seed.Save("chunk," + px + "," + py + ".jpeg", ImageFormat.Jpeg);
-			}
-		}
-
-		public static void Perlin(int px, int py)
-		{
-			Bitmap seed = new Bitmap("chunk," + px + "," + py + ".jpeg");
-
-			Bitmap output = new Bitmap(256, 256, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
-
-
-			for (int y = 0; y < 256; y++)
-			{
-				for (int x = 0; x < 256; x++)
-				{
-					double noise = 0.0;
-					double scale = 1.0;
-					double acc = 0.0;
-
-					for (int o = 0; o < 8; o++)
-					{
-						int pitch = 256 >> o;
-						int sampleX1 = (x / pitch) * pitch;
-						int sampleY1 = (y / pitch) * pitch;
-
-
-						int sampleX2 = (sampleX1 + pitch);
-						int sampleY2 = (sampleY1 + pitch);
-
-						sampleX2 = (sampleX2 == 256) ? 255 : sampleX2;
-						sampleY2 = (sampleY2 == 256) ? 255 : sampleY2;
-
-						double Xblend = (double)(x - sampleX1) / (double)pitch;
-						double Yblend = (double)(y - sampleY1) / (double)pitch;
-
-						// interpolate between the two points
-						double Tsample = ((1 - Xblend) * ((double)seed.GetPixel(sampleX1, sampleY1).R / 255.0)) + (Xblend * ((double)seed.GetPixel(sampleX2, sampleY1).R / 255.0));
-						double Bsample = ((1 - Xblend) * ((double)seed.GetPixel(sampleX1, sampleY2).R / 255.0)) + (Xblend * ((double)seed.GetPixel(sampleX2, sampleY2).R / 255.0));
-
-						noise += (((1 - Yblend) * Tsample) + (Yblend * Bsample)) * scale;
-						acc += scale;
-						scale = scale * 0.6;
-					}
-
-					noise = noise / acc;
-
-					noise = noise * 255.0;
-
-					output.SetPixel(x, y, Color.FromArgb(255, (int)(noise), (int)(noise), (int)(noise)));
-				}
-			}
-			seed.Dispose();
-			output.Save("chunk," + px + "," + py + ".jpeg", System.Drawing.Imaging.ImageFormat.Jpeg);
-			output.Dispose();
-		}
-
 		/* Steps for map generation
 		 * - define biomes
 		 * - set heightmap for each biome
@@ -109,6 +33,267 @@ namespace MapGenerator
 												 Color.FromArgb(5 << 5, Color.AntiqueWhite),
 												 Color.FromArgb(6 << 5, Color.Yellow),
 												 Color.FromArgb(7 << 5, Color.Firebrick)};
+
+
+
+
+
+		public static Point[,] Poisson(int depth, Bitmap source, bool isFirstItteration)
+		{
+			var r = depth >> 1;
+			var k = 30;
+
+			var cellSize = (r >> 1) * Math.Sqrt(2);
+			var cellinverse = Math.Sqrt(2) / r;
+			var size = (int)Math.Ceiling(source.Width * cellinverse);
+
+			var grid = new Point[size, size];
+			for(int i = 0; i < size; i++)
+			{
+				for(int j = 0; j < size; j++)
+				{
+					grid[i, j] = new Point(-1, -1);
+				}
+			}
+
+			var sample0 = (isFirstItteration) ? new Point(source.Width >> 1, source.Height >> 1) : new Point(rng.Next(source.Width), rng.Next(source.Height));
+			grid[(int)(sample0.X * cellinverse), (int)(sample0.Y * cellinverse)] = sample0;
+			var active = new List<Point> { sample0 };
+
+			if (isFirstItteration)
+			{
+				source.SetPixel(sample0.X, sample0.Y, biomeColor[(int)biome.badlands]);
+			}
+
+			while(active.Count > 0)
+			{
+				var workingIndex = rng.Next(active.Count);
+				var working = active[workingIndex];
+
+				var foundValid = false;
+				for(int i = 0; i < k; i++)
+				{
+					var sa = 2 * rng.NextDouble() * Math.PI;
+					var sr = (rng.NextDouble() * r) + r;
+
+					var sx = sr * Math.Cos(sa) + working.X;
+					var sy = sr * Math.Sin(sa) + working.Y;
+
+					sx = Math.Max(0, sx);
+					sy = Math.Max(0, sy);
+
+					sx = Math.Min(sx, (depth << 1));
+					sy = Math.Min(sy, (depth << 1));
+
+
+					var sample = new Point((int)sx, (int)sy);
+					var sgrid = new Point((int)Math.Floor(sample.X * cellinverse), (int)Math.Floor(sample.Y * cellinverse));
+
+					var isValid = true;
+
+					for (int j = -1; j <= 1; j++)
+					{
+						for (int h = -1; h <= 1; k++)
+						{
+							if ((j + sgrid.X) >= 0 && (j + sgrid.X) < size && (h + sgrid.Y) >= 0 && (h + sgrid.Y) < size)
+							{
+								var neighbor = grid[sgrid.X + j, sgrid.Y + h];
+
+								if (neighbor.X != -1)
+								{
+									var dist = ((neighbor.X - sample.X) * (neighbor.X - sample.X)) + ((neighbor.Y - sample.Y) * (neighbor.Y - sample.Y));
+									if (dist < (r * r))
+									{
+										isValid = false;
+									}
+								}
+							}
+						}
+					}
+
+					if(isValid)
+					{
+						foundValid = true;
+						grid[sgrid.X, sgrid.Y] = sample;
+						active.Add(sample);
+					}
+				}
+				
+				if(!foundValid)
+				{
+					active.RemoveAt(workingIndex);
+				}
+			}
+
+			for(int i = 0; i < size; i++)
+			{
+				for(int j = 0; j < size; j++)
+				{
+					if (grid[i, j] != new Point(-1, -1))
+					{
+						if (isFirstItteration)
+						{
+							var dist = ((grid[i, j].X - sample0.X) * (grid[i, j].X - sample0.X)) + ((grid[i, j].Y - sample0.Y) * (grid[i, j].Y - sample0.Y));
+
+							var isOut = (dist > (depth * depth)) || grid[i, j] == sample0;
+
+							if (isOut)
+							{
+								source.SetPixel(grid[i, j].X, grid[i, j].Y, biomeColor[(int)biome.badlands]);
+							}
+							else if (-grid[i, j].Y >= Math.Abs(grid[i, j].X))
+							{
+								source.SetPixel(grid[i, j].X, grid[i, j].Y, biomeColor[(int)biome.forest]);
+							}
+							else if (grid[i, j].X > Math.Abs(grid[i, j].Y))
+							{
+								source.SetPixel(grid[i, j].X, grid[i, j].Y, biomeColor[(int)biome.plains]);
+							}
+							else if (-grid[i, j].X > Math.Abs(grid[i, j].Y))
+							{
+								source.SetPixel(grid[i, j].X, grid[i, j].Y, biomeColor[(int)biome.swamp]);
+							}
+							else
+							{
+								source.SetPixel(grid[i, j].X, grid[i, j].Y, biomeColor[(int)biome.desert]);
+							}
+						}
+						else
+						{
+							if(rng.NextDouble() < 0.03)
+							{
+								source.SetPixel(grid[i, j].X, grid[i, j].Y, biomeColor[(int)biome.ruins]);
+							}
+							else if(rng.NextDouble() < 0.03)
+							{
+								source.SetPixel(grid[i, j].X, grid[i, j].Y, biomeColor[(int)biome.mountain]);
+							}
+							else if (rng.NextDouble() < 0.03)
+							{
+								source.SetPixel(grid[i, j].X, grid[i, j].Y, biomeColor[(int)biome.river]);
+							}
+							else { }
+						}
+					}
+				}
+			}
+
+			return grid;
+		}
+
+		public static void Voronoi(Point[,] seeds, Bitmap source, bool isFirstItteration, int depth)
+		{
+
+			var r = depth >> 1;
+
+			var cellSize = (r >> 1) * Math.Sqrt(2);
+			var cellinverse = Math.Sqrt(2) / r;
+			var size = (int)Math.Ceiling(source.Width * cellinverse);
+
+			for (int x = 0; x < source.Width; x++)
+			{
+				for (int y = 0; y < source.Height; y++)
+				{
+					var curPoint = new Point();
+
+					var curgrid = new Point((int)Math.Floor(x * cellinverse), (int)Math.Floor(y * cellinverse));
+
+					int curDistance = int.MaxValue;
+
+					for (int i = -2; i <= 2; i++)
+					{
+						for (int j = -2; j <= 2; j++)
+						{
+							if ((i + curgrid.X) >= 0 && (i + curgrid.X) < size && (j + curgrid.Y) >= 0 && (j + curgrid.Y) < size)
+							{
+								var neighbor = seeds[curgrid.X + i, curgrid.Y + j];
+
+								if (neighbor != new Point(-1, -1))
+								{
+									var wdist = ((neighbor.X - x) * (neighbor.X - x)) + ((neighbor.Y - y) * (neighbor.Y - y));
+
+									if (wdist < curDistance)
+									{
+										curPoint = neighbor;
+										curDistance = wdist;
+									}
+								}
+							}
+						}
+					}
+					source.SetPixel(x, y, source.GetPixel(curPoint.X, curPoint.Y));
+				}
+			}
+		}
+
+		public static void Colorize(Bitmap source)
+		{
+			for (int x = 0; x < source.Width; x++)
+			{
+				for (int y = 0; y < source.Height; y++)
+				{
+					switch (source.GetPixel(x, y).A >> 5)
+					{
+						case 0:
+							source.SetPixel(x, y, Color.FromArgb(255, biomeColor[0]));
+							break;
+						case 1:
+							source.SetPixel(x, y, Color.FromArgb(255, biomeColor[1]));
+							break;
+						case 2:
+							source.SetPixel(x, y, Color.FromArgb(255, biomeColor[2]));
+							break;
+						case 3:
+							source.SetPixel(x, y, Color.FromArgb(255, biomeColor[3]));
+							break;
+						case 4:
+							source.SetPixel(x, y, Color.FromArgb(255, biomeColor[4]));
+							break;
+						case 5:
+							source.SetPixel(x, y, Color.FromArgb(255, biomeColor[5]));
+							break;
+						case 6:
+							source.SetPixel(x, y, Color.FromArgb(255, biomeColor[6]));
+							break;
+						default:
+							source.SetPixel(x, y, Color.FromArgb(255, biomeColor[7]));
+							break;
+					}
+				}
+			}
+			source.Save("test.bmp");
+		}
+
+		public static void GenerateBiomes(int depth)
+		{
+			/* Generation steps
+			 * 
+			 * - create source image
+			 * - generate poisson seed points for first stage corse biome definition
+			 * - define biomes for initial seed points
+			 *   - forest: -y >= |x|
+			 *   - plains: x > |y|
+			 *   - swamp: -x > |y|
+			 *   - desert: y >= |x|
+			 *   - badlands: d > depth, or d < (depth >> 2)
+			 * - generate voronoi from seed points
+			 *   - NOTE: distance formula between points: (x2 - x1) ^ 2 + (y2 - y1) ^ 2
+			 *   - NOTE: distance formula between point and circle: |(x - ox) ^ 2 + (y - oy) ^ 2 - r|
+			 * - while current desired radius is > 4, recursively generate seed Poisson points fram previouse itteration, and generate Voronoi from new seeds
+			 * - colorize the image
+			 */
+			using (var output = new Bitmap((depth << 1) + 1, (depth << 1) + 1))
+			{
+				var seeds = Poisson(depth, output, true);
+
+				//Voronoi(seeds, output, true, depth);
+
+				Colorize(output);
+			}
+
+
+		}
+
 
 		public static void Generate(int depth, int points)
 		{
@@ -135,18 +320,6 @@ namespace MapGenerator
 
 			using (var output = new Bitmap((depth << 1) + 1, (depth << 1) + 1))
 			{
-				//Point[] seeds = new Point[points];
-
-				//for (int i = 0; i < points; i++)
-				//{
-				//	seeds[i] = new Point(rng.Next(0, (depth << 1) + 1), rng.Next(0, (depth << 1) + 1));
-
-				//	var Biome = biomeColor[rng.Next(8)];
-
-				//	output.SetPixel(seeds[i].X, seeds[i].Y, Color.FromArgb(Biome.A, 0, 0, 0));
-				//}
-
-
 				int percent = 0;
 
 				/* Poisson disk sampling
@@ -155,11 +328,11 @@ namespace MapGenerator
 				 * k: the number of tries each step, typically 30
 				 */
 
-				var pr = 128;
+				var pr = 100;
 				var pk = 30;
 
 				/* step 0:
-				 * + initialize n dimentional array (grid) to store samples and speeing up spacial calculations [in this case n = 2]
+				 * + initialize n dimentional array (grid) to store samples and speeding up spacial calculations [in this case n = 2]
 				 * + set cell size to r * sqrt(n) / n [in this case n = 2]
 				 * + populate array with (-1,-1)
 				 */
@@ -183,7 +356,7 @@ namespace MapGenerator
 				 * + initialize active array with this starting point's index
 				 */
 
-				var sample0 = new Point(rng.Next(output.Width), rng.Next(output.Height));
+				var sample0 = new Point((output.Width >> 1), (output.Height >> 1));
 
 				pgrid[(int)Math.Floor(sample0.X * pcellinverse), (int)Math.Floor(sample0.Y * pcellinverse)] = sample0; // <- this will add the chosen sample into the grid at the proper location.
 				var pactive = new List<Point> { sample0 };
@@ -191,9 +364,9 @@ namespace MapGenerator
 				/* Step 2:
 				 * + While active is not empty, pick a random index i from active
 				 * + generate up to k points between r and 2r away from selected active sample
-				 * - for each generated sample, check to see if it is within the range of r of any other sample, using the grid to narrow the search to only neighbors
-				 * - if generated point is valid, add it to the active list
-				 * - if no valid samples were generated, remove i from active list
+				 * + for each generated sample, check to see if it is within the range of r of any other sample, using the grid to narrow the search to only neighbors
+				 * + if generated point is valid, add it to the active list
+				 * + if no valid samples were generated, remove i from active list
 				 */
 				 
 				var seeds = new List<Point>();
@@ -275,14 +448,30 @@ namespace MapGenerator
 					for (int y = 0; y < output.Height; y++)
 					{
 						var curPoint = new Point();
+
+						var curgrid = new Point((int)Math.Floor(x * pcellinverse), (int)Math.Floor(y* pcellinverse));
+
 						int curDistance = int.MaxValue;
 
-						for (int i = 0; i < seeds.Count; i++)
+						for (int i = -2; i <= 2; i++)
 						{
-							if (((seeds[i].X - x) * (seeds[i].X - x) + (seeds[i].Y - y) * (seeds[i].Y - y)) <= curDistance)
+							for (int j = -2; j <= 2; j++)
 							{
-								curDistance = ((seeds[i].X - x) * (seeds[i].X - x) + (seeds[i].Y - y) * (seeds[i].Y - y));
-								curPoint = seeds[i];
+								if ((i + curgrid.X) >= 0 && (i + curgrid.X) < psize && (j + curgrid.Y) >= 0 && (j + curgrid.Y) < psize)
+								{
+									var neighbor = pgrid[curgrid.X + i, curgrid.Y + j];
+
+									if (neighbor != new Point(-1, -1))
+									{
+										var wdist = ((neighbor.X - x) * (neighbor.X - x)) + ((neighbor.Y - y) * (neighbor.Y - y));
+
+										if (wdist < curDistance)
+										{
+											curPoint = neighbor;
+											curDistance = wdist;
+										}
+									}
+								}
 							}
 						}
 
@@ -345,7 +534,7 @@ namespace MapGenerator
 			sw.Start();
 			Console.WriteLine("Starting generation, please wait, will take some time.");
 
-			Generate(720, 10);
+			GenerateBiomes(720);
 
 			sw.Stop();
 			Console.WriteLine("Finished generation in: " + sw.Elapsed);
